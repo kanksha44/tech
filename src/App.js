79dc376e45a49945858259c0200d7b4611e522ca
kanksha4496 <kanksha44@gmail.com>
2023-04-19 22:03:41 +0530

    1st assignment 1part

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Benefits of using React...</h1>
+        <ol>
+          <li>Component-based architecture</li>
+          <li>Virtual DOM for efficient updates</li>
+          <li>Rich ecosystem and community</li>
+          <li>Cross-platform developement</li>
+          <li>Strong community support</li>
+        </ol>
+        <button>Get Started</button>
       </header>
     </div>
   );
